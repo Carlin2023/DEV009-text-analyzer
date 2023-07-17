32b303efd0f5ea7b2e8162af112993fe49aea79d
Carolina Alvarez <caroalvm@gmail.com>
2023-07-17 17:45:49 -0500

    Fin del Proyecto

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,56 @@
-const analyzer = {  
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
+const analyzer = {
+
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    //Conteo de caracteres
+    return text.length;
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const textoSinEspacios = text.replace(/\s/g, "").replace(/[^\w\s]|_/g,"")
+    const CantidadCaracteres = textoSinEspacios.length;
+    return CantidadCaracteres;
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getWordCount: (text) => {
+    // conteo de palabras 
+    return text.trim().split(" ").length;
   },
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    //conteo de números
+    //const contarNumerosEnTexto = (text)
+    let contador = 0;
+    const palabras = text.split(" ");
+    for (let i = 0; i < palabras.length; i++) {
+      if (!isNaN(palabras[i])) {
+        contador = contador+1
+        //contarNumerosEnTexto;
+      }
+    }
+    return contador;
   },
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
-};
+    const sumaDeNumeros = text.split(' ');
+    let sumaNumeros = 0;
+    for (let i = 0; i < sumaDeNumeros.length; i++) {
+   
+      if (!isNaN(sumaDeNumeros[i])) {
+        sumaNumeros = sumaNumeros + parseFloat(sumaDeNumeros[i]);
+      }
 
-export default analyzer;
+    }
+    return sumaNumeros;
+  },
+
+  getAverageWordLength: (text) => {
+    const longitudMedia = text.split(' ');
+    let sumaLongitudMedia = 0;
+    for (let i = 0; i < longitudMedia.length; i++) {
+      sumaLongitudMedia += longitudMedia[i].length;
+    }
+    return Math.round(sumaLongitudMedia / longitudMedia.length * 100) / 100;
+    //return sumaLongitudMedia;
+  },
+}
+export default analyzer
--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const textarea = document.querySelector("textarea");
+textarea.addEventListener("input", (evento) => {
+  const text = evento.target.value
+
+  //código de la función getCharacterCount, RECUENTO DE CARACTERES 
+  const suma = analyzer.getCharacterCount(text);
+  const caracteresSpan = document.getElementById("caracteres");
+  caracteresSpan.innerHTML = suma
+
+  //código de la función getCharacterCountExcluidingSpaces, RECUENTO DE CARACTERES SIN ESPACIOS 
+  const recuento = analyzer.getCharacterCountExcludingSpaces(text);
+  const caracteresSinEspacioSpan = document.getElementById("caracteresSinEspacio")
+  caracteresSinEspacioSpan.innerHTML = recuento
+
+  //código de la función getWordCount, RECUENTO DE PALABRAS
+  const conteo = analyzer.getWordCount(text);
+  const palabrasSpan = document.getElementById("palabras");
+  palabrasSpan.innerHTML = conteo
+
+  //código de la función getNumberCount CONTEO DE NUMEROS
+  const conteoNumeros = analyzer.getNumberCount(text);
+  const numerosSpan = document.getElementById("numeros");
+  numerosSpan.innerHTML = conteoNumeros
+
+  //código de la función getNumberSum SUMA DE NUMEROS
+  const sumaNumeros = analyzer.getNumberSum(text);
+  const sumaNumerosSpan = document.getElementById("sumaNumeros");
+  sumaNumerosSpan.innerHTML = sumaNumeros
+
+  //código de la función getAverageWordLength CALCULO DE LONGITUD PROMEDIO DE LAS PALABRAS 
+  const calcularLongitudes = analyzer.getAverageWordLength(text);
+  const promedioLongitudSpan = document.getElementById("promedioLongitud");
+  promedioLongitudSpan.innerHTML = calcularLongitudes
+})
+
+//borrar el contenido de la caja de texto
+//function limpiarTexto() {
+//textarea.value = ""; // borrar el contenido de la caja de texto
+//}
+//botonLimpiar.addEventListener("click", limpiarTexto);
